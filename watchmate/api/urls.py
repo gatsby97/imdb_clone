101fd9d9670b1894de0f01ec19803571d1c4b376
gatsby97 <mk9999666@gmail.com>
2022-12-04 08:03:24 +0530

    -a

--- watchmate/api/urls.py
+++ watchmate/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path,include
 #from watchmate.api.views import movie_list,movie_detail
-from watchmate.api.views import WatchListAV, WatchDetailAV,StreamPlatformListAV,StreamPlatformDetailAV
+from watchmate.api.views import WatchListAV, WatchDetailAV,StreamPlatformListAV,StreamPlatformDetailAV,ReviewListAV,ReviewDetailAV
 
 # in class based views class name is passed with as_view() method
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name= 'movie-list'),       
     path('<int:pk>', WatchDetailAV.as_view(), name='movie-detail'),
     path('stream',StreamPlatformListAV.as_view(), name = 'streamplatform-list'),
-    path('stream/<int:pk>',StreamPlatformDetailAV.as_view(), name = 'streamplatform-detail')
+    path('stream/<int:pk>',StreamPlatformDetailAV.as_view(), name = 'streamplatform-detail'),
+    path('reviews',ReviewListAV.as_view(), name='review-list'),
+    path('reviews/<int:pk>',ReviewDetailAV.as_view(),name='review-detail')
     
     ]
--- watchmate/api/views.py
+++ watchmate/api/views.py
@@ -1,12 +1,14 @@
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import generics
+from rest_framework import mixins
 #from rest_framework.decorators import api_view
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
-from watchmate.api.serializers import WatchListSerializer,StreamPlatformSerializer #replace Movieserializer with updated WatchListSerializer
+from watchmate.api.serializers import WatchListSerializer,StreamPlatformSerializer,ReviewSerializer #replace Movieserializer with updated WatchListSerializer
 # Create your views here.
 from rest_framework.views import APIView
-from watchmate.models import WatchList,StreamPlatform # Movies models is replaced with watchlist serializer 
+from watchmate.models import WatchList,StreamPlatform,Review # Movies models is replaced with watchlist serializer 
 
 
 # views are of 2 types class based and function based views 
@@ -51,6 +53,25 @@ def movie_detail(request,pk):
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     """
+class ReviewDetailAV(mixins.RetrieveModelMixin,generics.GenericAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    def get(self, request , *args, **kwargs):
+        return self.retrieve(request,*args,**kwargs)
+    
+    
+       
+    
+
+class ReviewListAV(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    
+    def get(self, request , *args, **kwargs):
+        return self.list(request,*args,**kwargs)
+    
+    def post(self,request, *args, **kwargs):
+        return self.create(request,*args,**kwargs)
 
 class WatchListAV(APIView):
     
@@ -70,6 +91,7 @@ class WatchListAV(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
         
 class WatchDetailAV(APIView):
     # status object to pass status codes from status clss from DRF
@@ -133,3 +155,5 @@ class StreamPlatformDetailAV(APIView):
         deleted_obj = StreamPlatform.objects.get(pk=pk)
         deleted_obj.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
